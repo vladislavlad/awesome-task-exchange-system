17156b75cd13abd1e02badbb48b813c2092f133e
Vladislav Kartashov <kartashovladislav@gmail.com>
2024-01-09 23:52:29 +0300

    Task Service: Task AssignedList Dto

--- task-service/src/main/kotlin/toughdevschool/ates/task/api/TaskAssignedListDto.kt
+++ task-service/src/main/kotlin/toughdevschool/ates/task/api/TaskAssignedListDto.kt
@@ -1,5 +1,6 @@
 package toughdevschool.ates.task.api
 
+import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.data.domain.Pageable
 
 object TaskAssignedListDto {
@@ -8,6 +9,7 @@ object TaskAssignedListDto {
         val pageable: Pageable
     )
 
+    @Schema(name = "TaskAssignedListResponse")
     data class Response(
         val tasks: List<TaskDto.Response>
     )
--- task-service/src/main/kotlin/toughdevschool/ates/task/api/rest/TaskController.kt
+++ task-service/src/main/kotlin/toughdevschool/ates/task/api/rest/TaskController.kt
@@ -1,6 +1,7 @@
 package toughdevschool.ates.task.api.rest
 
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.GetMapping
@@ -10,13 +11,12 @@ import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import toughdevschool.ates.task.api.TaskAssignedListDto
 import toughdevschool.ates.task.api.TaskDto
 import toughdevschool.ates.task.domain.task.assigned.business.TaskAssignedListApi
 import toughdevschool.ates.task.domain.task.complete.business.TaskCompleteApi
 import toughdevschool.ates.task.domain.task.crud.business.TaskCrudApi
 import toughdevschool.ates.task.domain.task.reassign.business.TaskReassignApi
-import jakarta.validation.Valid
-import toughdevschool.ates.task.api.TaskAssignedListDto
 
 @Tag(name = "Tasks")
 @RestController
