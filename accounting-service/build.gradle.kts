ad99f578e64df31c4aa926984957597b277156e8
Vladislav Kartashov <kartashovladislav@gmail.com>
2024-04-26 21:35:53 +0300

    platform 0.5.3, spring boot 3.2.5

--- accounting-service/build.gradle.kts
+++ accounting-service/build.gradle.kts
@@ -3,20 +3,20 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
-    id("org.springframework.boot") version "3.2.3"
+    id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "toughdevschool.ates"
-version = "0.3.3"
+version = "0.3.4"
 
 repositories {
     mavenLocal()
     mavenCentral()
 }
 
-val springCloudVersion = "2023.0.0"
-val platformVersion = "0.5.2"
+val springCloudVersion = "2023.0.1"
+val platformVersion = "0.5.3"
 val micrometerVersion = "1.2.4"
 
 dependencies {
@@ -32,7 +32,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-stream")
     implementation("org.springframework.cloud:spring-cloud-stream-binder-kafka")
     implementation("org.springframework.kafka:spring-kafka")
-    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.3.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.5.0")
 
     // Micrometer dependencies
     implementation(platform("io.micrometer:micrometer-tracing-bom:$micrometerVersion"))
@@ -58,8 +58,8 @@ dependencies {
     implementation("org.postgresql:r2dbc-postgresql")
     runtimeOnly("org.postgresql:postgresql")
 
-    implementation("io.arrow-kt:arrow-core:1.2.1")
-    implementation("io.github.oshai:kotlin-logging:6.0.3")
+    implementation("io.arrow-kt:arrow-core:1.2.4")
+    implementation("io.github.oshai:kotlin-logging:6.0.9")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
